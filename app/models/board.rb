277d4ecc6814c9e5470c761f1c33895007d0eaf1
suhasnisingh <165236685+suhasnisingh@users.noreply.github.com>
2024-05-04 08:06:53 +0000

    complete

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,7 +6,9 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
+  belongs_to(:owner, class_name: "User", foreign_key: "user_id")
 end
